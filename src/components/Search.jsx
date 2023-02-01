b76e44da2171d031470a3e22164d75267be1e582
Carlos Marfil <carlos.marfil.r@gmail.com>
2023-02-01 12:26:12 +0000

    used axios to send getImages request

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
+import getImages from "../requests/getImages";
 import "../styles/search.css";
 
 const Search = () => {
   const [value, setValue] = useState();
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getImages(value);
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <input
         className="search-box"
         type="text"
---
+++ src/requests/getImages.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from "axios";
+
+const getImages = (query) => {
+  if (!query) {
+    return Promise.resolve([]);
+  }
+  return axios
+    .get(`https://images-api.nasa.gov/search?q=${query}`)
+    .then((response) => {
+      const imageResults = response.data.collection.items;
+
+      const parsedImages = imageResults.filter(
+        (i) => i.data[0].media_type === "image"
+      );
+
+      const images = parsedImages.map((image) => image.links[0].href);
+      return images;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export default getImages;
