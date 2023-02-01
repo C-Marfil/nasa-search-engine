c28b41195530c139c56559bba6075250249d2e34
Carlos Marfil <carlos.marfil.r@gmail.com>
2023-02-01 11:17:41 +0000

    added button, styles and updated Snaphots

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/search.css";
 
 const Search = () => {
+  const [value, setValue] = useState();
+
   return (
-    <>
-      <input className="search-box" type="text" placeholder="Search here" />
+    <form className="form">
+      <input
+        className="search-box"
+        type="text"
+        placeholder="Search here"
+        onChange={(e) => setValue(e.target.value)}
+      />
       <button className="button" type="submit">
         GO
       </button>
-    </>
+    </form>
   );
 };
 
