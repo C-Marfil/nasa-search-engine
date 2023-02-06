a4ebb178f24c5a6f65b2b3960650efe587802450
Carlos Marfil <carlos.marfil.r@gmail.com>
2023-02-06 17:50:30 +0000

    style corrections

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,20 +4,23 @@ import "../styles/home.css";
 
 const Home = () => {
   return (
-    <MovingComponent
-      className="moving-words"
-      type="pulse"
-      duration="2000ms"
-      delay="1s"
-      direction="alternate-reverse"
-      timing="ease"
-      id="pulse"
-      iteration="infinite"
-      fillMode="none"
-    >
-      Welcome, use the Search Bar above to find any pictures held in the public
-      NASA archives.
-    </MovingComponent>
+    <div className="homediv">
+      <MovingComponent
+        className="moving-words"
+        type="pulse"
+        duration="2000ms"
+        delay="1s"
+        direction="alternate-reverse"
+        timing="ease"
+        id="pulse"
+        iteration="infinite"
+        fillMode="none"
+      >
+        Welcome, use the Search Bar above to find any pictures held in the
+        public NASA archives.
+      </MovingComponent>
+      <hr />
+    </div>
   );
 };
 
